cfe57ee8ce20be52dd35edd0f909f81c74823521
Fecske Márk <mrk.fecske@gmail.com>
2023-02-28 13:07:09 +0100

    második feladatrész

--- javascript.js
+++ javascript.js
@@ -1,9 +1,18 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    document.getElementById("gomb").addEventListener("click",function(){
+        const input1=document.getElementById("input1").value;
+        const input2=document.getElementById("input2").value;
+        const visszajelzes = document.getElementById("visszajelzes")
+        visszajelzes.innerHTML=nagyobb(input1,input2)
+    })
+})
+
 function melyiknagyobb(szam1,szam2) {
     if (szam1>szam2) {
-        return szam1
+        return "Az első szám a nagyobb"+szam1
     }
     else if (szam1<szam2) {
-        return szam2
+        return "A második szám a nagyobb"+szam2
     }
     else {
         return "A két szám egyenlő"
