e024a751d8a37a2b068239898d73b0104f6ede1a
Fecske Márk <mrk.fecske@gmail.com>
2023-02-28 13:00:18 +0100

    első feladatrész

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function melyiknagyobb(szam1,szam2) {
+    if (szam1>szam2) {
+        return szam1
+    }
+    else if (szam1<szam2) {
+        return szam2
+    }
+    else {
+        return "A két szám egyenlő"
+    }
+}
