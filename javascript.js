ae8f72e212b49a90c130c61ed65a2df70eb3fd41
Fecske Márk <mrk.fecske@gmail.com>
2023-02-28 13:21:26 +0100

    Kész feladat

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
 const lista=[];
-const max = 0;
 document.addEventListener("DOMContentLoaded",()=>{
     document.getElementById("Hozzaad").addEventListener("click",function(){
         const input1=document.getElementById("input1").value;
         lista.push(input1)
     });
     document.getElementById("kiIr").addEventListener("click",function(){
-        document.getElementById("visszajelzes").innerHTML="A legnagyobb szám a felsorolásból: "+melyiknagyobb(lista,max)
+        const min=input1=document.getElementById("input1").value;
+        const max=input1=document.getElementById("input1").value;
+        document.getElementById("visszajelzes").innerHTML="A legnagyobb szám a felsorolásból: "+melyiknagyobb(lista,max)+"A legkisebb szám a felsorolásból: "+melyikkisebb(lista,min)
     });
 });
 
 function melyiknagyobb(lista,max) {
-    max=0
     for (let index = 0; index<lista.length;index++){
         const element=lista[index];
         if (element>max){
@@ -19,4 +19,13 @@ function melyiknagyobb(lista,max) {
         }
     }
     return max
+}
+function melyikkisebb(lista,min) {
+    for (let index =0; index<lista.length; index++){
+        const element=lista[index];
+        if(elementt<min){
+            min=element
+        }
+    }
+    return min
 }
